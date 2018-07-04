58bb91de27555d253f40adcdffe526596a670e52
Terry Wang <terry.wang@accenture.com>
2018-07-03 22:39:31 -0400

    first commit

---
+++ YIMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YIMenu
+//
+//  Created by Yi Wang on 7/3/18.
+//  Copyright © 2018 Yi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
